0198cf9f3ef7343a3c525cf30894da2ddcf61df1
vaquinel dias jegros <108008624+vaquineljegros@users.noreply.github.com>
2024-10-10 10:53:52 -0300

    1

---
+++ app.js
@@ -0,0 +1,13 @@
+funtion criaCartao(categoria,oergunta,resosta){
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+
+    cartao.innerHTML = '
+    <div class="cartao__conteudo">
+    </h3>s{categoria}</h3>
+    <div class="cartao__conteudo__pergunta">
+        <p>s{pergunta}</p>
+     </div>
+     <div class
+}
