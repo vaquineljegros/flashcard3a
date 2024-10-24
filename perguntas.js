5111df2aa1b44677f53507f7fc01cf136f858eb5
vaquinel dias jegros <108008624+vaquineljegros@users.noreply.github.com>
2024-10-24 10:11:57 -0300

    1

--- perguntas.js
+++ perguntas.js
@@ -15,3 +15,9 @@ criaCartao(
     'O que é uma funçao?',
     'Uma funçao é um bloco de codigo que executa alguma tarefa'
 )
+
+criaCartao(
+    'Lingua inglesa'
+    'Como se diz oi em Ingles'
+    'Oi em ingles é HI (RAI)'
+)
